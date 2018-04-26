7be452eda4ef8ed2fca14015861897b049e56ec2
Krzysztof Syska <ksyska@pl.sii.eu>
2018-04-26 12:54:48 +0200

    [velka] JAV-15 created test for debtor form

---
+++ src/test/java/eu/sii/pl/velka/view/StartFormTest.java
@@ -0,0 +1,48 @@
+package eu.sii.pl.velka.view;
+
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Component;
+import com.vaadin.ui.TextField;
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class StartFormTest {
+
+    @Test
+    public void shouldCreateFormWhichHaveThreeFieldsAndButton(){
+        //given
+        int numberOfTextFields = 0;
+        int numberOfButtons = 0;
+
+        //when
+        StartForm startForm = new StartForm((Button.ClickListener) event -> {});
+
+        //then
+        for (int i = 0; i < startForm.getComponentCount(); i++) {
+            Component c = startForm.getComponent(i);
+            if (c instanceof TextField) numberOfTextFields++;
+            if (c instanceof Button) numberOfButtons++;
+        }
+        Assert.assertEquals(3, numberOfTextFields);
+        Assert.assertEquals(1, numberOfButtons);
+    }
+
+    @Test
+    public void checkLabelOfButton(){
+        //given
+        String labelOnSubmitButton = "";
+
+        //when
+        StartForm startForm = new StartForm((Button.ClickListener) event -> {});
+
+        //then
+        for (int i = 0; i < startForm.getComponentCount(); i++) {
+            Component c = startForm.getComponent(i);
+            if (c instanceof Button){
+                labelOnSubmitButton = c.getCaption();
+            }
+        }
+        Assert.assertEquals("Submit", labelOnSubmitButton);
+    }
+}
