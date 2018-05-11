b23d68b4dfe53a2ed23e4f6e24518dad3b098ebb
Krzysztof Syska <ksyska@pl.sii.eu>
2018-05-11 10:59:38 +0200

    [velka] Refactored tests

--- src/test/java/eu/sii/pl/velka/view/StartFormTest.java
+++ src/test/java/eu/sii/pl/velka/view/StartFormTest.java
@@ -5,10 +5,7 @@ import com.vaadin.ui.Component;
 import com.vaadin.ui.TextField;
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
 public class StartFormTest {
 
     @Test
