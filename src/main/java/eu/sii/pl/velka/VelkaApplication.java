103acd02e8330a3f9a79a412fef32471e0a8afb4
Krzysztof Syska <ksyska@pl.sii.eu>
2018-05-11 14:18:49 +0200

    [velka] Added different logging levels to console and to file

--- src/main/java/eu/sii/pl/velka/VelkaApplication.java
+++ src/main/java/eu/sii/pl/velka/VelkaApplication.java
@@ -1,9 +1,7 @@
 package eu.sii.pl.velka;
 
-import eu.sii.pl.velka.model.Debtor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class VelkaApplication {
